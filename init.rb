26241b4f3d58b4f3b6a6f9a4ad3ab01b1f093021
Kang-min Liu <gugod@gugod.org>
2011-03-31 01:53:53 +0800

    rename any presence "Gollum Wiki" to just "Gollum".
    The term "Gollum Wiki" might confuse people and consider this module to be compatible
    with the built-in Wiki. In this sense it should work better to just consider the word "Gollum"
    to be a proper replacement of the word "Wiki".

--- init.rb
+++ init.rb
@@ -8,26 +8,26 @@ Dispatcher.to_prepare :redmine_model_dependencies do
   require_dependency 'user'
 end
 
-Redmine::Plugin.register :redmine_gollum_wiki do
+Redmine::Plugin.register :redmine_gollum do
   name 'Redmine Gollumn plugin'
   author 'Kang-min Liu <gugod@gugod.org>'
   description 'A gollum plugin for redmine'
   version '0.0.1'
   url 'https://github.com/gugod/redmine-gollumn/'
-  # author_url 'http://gugod.org'
+  author_url 'http://gugod.org'
 
   requires_redmine :version_or_higher => '1.1.0'
 
-  project_module :gollum_wiki do
-    permission :view_gollum_wiki,   :gollum => [:index, :show]
-    permission :add_gollum_wiki,    :gollum => [:new, :create]
-    permission :edit_gollum_wiki,   :gollum => [:edit, :update]
-    permission :delete_gollum_wiki, :gollum => [:destroy]
+  project_module :gollum do
+    permission :view_gollum_pages,   :gollum => [:index, :show]
+    permission :add_gollum_pages,    :gollum => [:new, :create]
+    permission :edit_gollum_pages,   :gollum => [:edit, :update]
+    permission :delete_gollum_pages, :gollum => [:destroy]
   end
 
-  menu :project_menu, :gollum, { :controller => :gollum, :action => :index }, :caption => 'Gollum Wiki', :before => :wiki, :param => :project_id
+  menu :project_menu, :gollum, { :controller => :gollum, :action => :index }, :caption => 'Gollum', :before => :wiki, :param => :project_id
 end
 
 Redmine::Activity.map do |activity|
-  activity.register :gollum_wiki
+  activity.register :gollum
 end
