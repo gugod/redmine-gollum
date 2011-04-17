ad4f1e7ff557a196c9b18c4d6a5f5ee627759b23
John Yani <vanuan@gmail.com>
2011-04-17 12:24:13 +0300

    Bug when creating new gollum wiki

--- init.rb
+++ init.rb
@@ -45,7 +45,7 @@ Redmine::Plugin.register :redmine_gollum do
     permission :edit_gollum_pages,   :gollum => [:edit, :update]
     permission :delete_gollum_pages, :gollum => [:destroy]
 
-    permission :manage_gollum_wiki, :gollum_wikis => [:index,:show, :update]
+    permission :manage_gollum_wiki, :gollum_wikis => [:index,:show, :create, :update]
   end
 
   menu :project_menu, :gollum, { :controller => :gollum, :action => :index }, :caption => 'Gollum', :before => :wiki, :param => :project_id
