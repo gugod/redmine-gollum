6d932da6e68a9a0aac3bbc917cbb7c58958d7422
John Yani <vanuan@gmail.com>
2012-06-23 23:44:55 +0300

    Redmine 2.0 compatible changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :gollum, :path_prefix => '/projects/:project_id'
-  map.resource  :gollum_wiki, :path_prefix => '/projects/:project_id'
+resources :projects do
+  resources :gollum
+  resource  :gollum_wiki
 end
