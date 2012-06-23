56bf9bb9e874a9ea7945f5d56fecea29b6821144
John Yani <vanuan@gmail.com>
2012-06-23 21:30:33 +0300

    Change initialization code for redmine 2 support

--- init.rb
+++ init.rb
@@ -1,18 +1,15 @@
 require 'grit'
 require 'gollum' # min version 1.2.0
 require 'redmine'
-require 'dispatcher'
 
-Dispatcher.to_prepare :redmine_model_dependencies do
+Rails.configuration.to_prepare do
   require_dependency 'project'
   require_dependency 'user'
   require_dependency 'projects_helper'
   require_dependency 'gollum_projects_helper_patch'
   require_dependency 'projects_controller'
   require_dependency 'gollum_projects_controller_patch'
-end
 
-Dispatcher.to_prepare :redmine_gollum do
   # project model should be patched before projects controller
   Project.send(:include, GollumProjectModelPatch) unless Project.included_modules.include?(GollumProjectModelPatch)
   ProjectsController.send(:include, GollumProjectsControllerPatch) unless ProjectsController.included_modules.include?(GollumProjectsControllerPatch)
@@ -26,13 +23,13 @@ Redmine::Plugin.register :redmine_gollum do
 
   # use git to get version name
   require 'grit'
-  repo = Grit::Repo.new("#{RAILS_ROOT}/vendor/plugins/redmine-gollum/.git")
+  repo = Grit::Repo.new("#{Rails.root}/plugins/redmine-gollum/.git")
   version repo.recent_tag_name('HEAD', :tags=>true)
 
   url 'https://github.com/gugod/redmine-gollum/'
   author_url 'http://gugod.org'
 
-  requires_redmine :version_or_higher => '1.1.0'
+  requires_redmine :version_or_higher => '2.0.2'
 
   settings :default => {
                        :gollum_base_path => Pathname.new(Rails.root + "gollum")
